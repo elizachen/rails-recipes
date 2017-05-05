f617752ae6104c5b90013e70c8c9b3b9e898b81c
eliza chen <elizachen34@gmail.com>
2017-05-05 15:19:15 +0800

    Multi-step wizard

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,11 +7,14 @@ class RegistrationsController < ApplicationController
   def create
     @registration = @event.registrations.new(registration_params)
     @registration.ticket = @event.tickets.find( params[:registration][:ticket_id] )
-    @registration.status = "confirmed"
+    # @registration.status = "confirmed"
+    @registration.status = "pending"
     @registration.user = current_user
+     @registration.current_step = 1
 
     if @registration.save
-      redirect_to event_registration_path(@event, @registration)
+      # redirect_to event_registration_path(@event, @registration)
+      redirect_to step2_event_registration_path(@event, @registration)
     else
       render "new"
     end
@@ -19,6 +22,54 @@ class RegistrationsController < ApplicationController
 
   def show
     @registration = @event.registrations.find_by_uuid(params[:id])
+  end
+
+    def step1
+    @registration = @event.registrations.find_by_uuid(params[:id])
+     @registration.current_step = 1
+  end
+
+  def step1_update
+    @registration = @event.registrations.find_by_uuid(params[:id])
+
+    if @registration.update(registration_params)
+      redirect_to step2_event_registration_path(@event, @registration)
+    else
+      render "step1"
+    end
+  end
+
+
+  def step2
+    @registration = @event.registrations.find_by_uuid(params[:id])
+     @registration.current_step = 2
+  end
+
+  def step2_update
+    @registration = @event.registrations.find_by_uuid(params[:id])
+
+    if @registration.update(registration_params)
+      redirect_to step3_event_registration_path(@event, @registration)
+    else
+      render "step2"
+    end
+  end
+
+  def step3
+    @registration = @event.registrations.find_by_uuid(params[:id])
+     @registration.current_step = 3
+  end
+
+  def step3_update
+    @registration = @event.registrations.find_by_uuid(params[:id])
+    @registration.status = "confirmed"
+
+    if @registration.update(registration_params)
+      flash[:notice] = "报名成功"
+      redirect_to event_registration_path(@event, @registration)
+    else
+      render "step3"
+    end
   end
 
   protected
