21c5c329db450731458f7b0454a6ca927ac50169
eliza chen <elizachen34@gmail.com>
2017-05-05 16:51:23 +0800

    ID Filter

--- app/controllers/admin/event_registrations_controller.rb
+++ app/controllers/admin/event_registrations_controller.rb
@@ -21,6 +21,10 @@ class Admin::EventRegistrationsController < ApplicationController
       @registrations = @registrations.where( "created_at <= ?", Date.parse(params[:end_on]).end_of_day )
     end
 
+    if params[:registration_id].present?
+     @registrations = @registrations.where( :id => params[:registration_id].split(",") )
+   end
+
 
     if params[:status].present? && Registration::STATUS.include?(params[:status])
       @registrations = @registrations.by_status(params[:status])
